42158adc9b7ce02f053915f149023a7fa6581c3d
Vu Cao <cdhvu@yahoo.com>
2021-09-19 07:49:34 -0700

    Completed all exercise 01

--- src/__tests__/01.js
+++ src/__tests__/01.js
@@ -2,7 +2,7 @@ import * as React from 'react'
 import {render, screen} from '@testing-library/react'
 import userEvent from '@testing-library/user-event'
 // import App from '../final/01'
-import App from '../exercise/01'
+import App from '../exercise/01.extra-1'
 
 test('typing a name shows a greeting', () => {
   render(<App />)
